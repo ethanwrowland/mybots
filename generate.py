9180893eca643c7c61adf8ab7295f09efc3c3c61
ethanwrowland <ethanwrowland@gmail.com>
2023-01-28 16:31:36 -0500

    made all the towers (very cool)

--- generate.py
+++ generate.py
@@ -1,7 +1,16 @@
 import pyrosim.pyrosim as pyrosim
 
 pyrosim.Start_SDF("boxes.sdf")
-pyrosim.Send_Cube(name="Box", pos=[0,0,.5] , size=[1,1,1])
-pyrosim.Send_Cube(name="Box2", pos=[1,0,1.5] , size=[1,1,1])
+
+
+def buildTower(x,y): #builds 1 of the towers centered at x,y
+    scale = 1.0
+    for i in range(1,10):
+        pyrosim.Send_Cube(name=("Box"+str(i)), pos=[x,y,i-.5] , size=[scale,scale,scale])
+        scale *= .9
+
+for i in range(-5,5,2):
+    for j in range(-5,5,2):
+        buildTower(i/2.0,j/2.0)
 
 pyrosim.End()
